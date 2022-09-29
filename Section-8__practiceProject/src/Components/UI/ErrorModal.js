0ac7f971202dc1d75421051e27220068d2add78e
devangga <devanggakertawijaya@gmail.com>
2022-09-29 09:53:49 +0700

    errorModal structure

--- Section-8__practiceProject/src/Components/UI/ErrorModal.js
+++ Section-8__practiceProject/src/Components/UI/ErrorModal.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import Card from ' ./Card'
+import Button from './Button'
 
 import styles from './ErrorModal.module.css'
 
 
 const ErrorModal = (props) => {
     return (
-        <div>
-
-        </div>
+        <Card className={styles.modal}>
+            <header className={styles.header}>
+                <h2>{props.errTitle}</h2>
+            </header>
+            <div className={styles.content}>
+                <p>{props.errMessage}</p>
+            </div>
+            <footer className={styles.action}>
+                <Button>
+                    Confirm
+                </Button>
+            </footer>
+        </Card>
     )
 }
 
