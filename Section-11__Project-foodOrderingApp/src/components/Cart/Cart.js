53985122acf7b13b60644693b6559c31fe04cfff
devangga <devanggakertawijaya@gmail.com>
2022-10-10 10:21:51 +0700

    Card.js : markup setup

--- Section-11__Project-foodOrderingApp/src/components/Cart/Cart.js
+++ Section-11__Project-foodOrderingApp/src/components/Cart/Cart.js
@@ -1,10 +1,32 @@
-import react from "react";
+import React from "react";
 
 import classes from './Cart.module.css'
 
 const Cart = (props) => {
-    return (
 
+    const cartItems = <ul className={classes['cart-items']}>
+        {
+            [{
+                id: 'c1',
+                name: 'Pempek',
+                amount: 3,
+                price: 15.99,
+            }].map(cartItem => <li>{cartItem.name}</li>)
+        }
+    </ul> 
+
+    return (
+        <div>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total amount</span>
+                <span>55.87</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes['button--alt']}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </div>
     )
 }
 
