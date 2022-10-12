282023387db0762023047d5ed454e35c04330517
devangga <devanggakertawijaya@gmail.com>
2022-10-12 11:12:37 +0700

    HeaderCartButton.js : Adding a bup animation with useEffect

--- Section-11__Project-foodOrderingApp/src/components/Layout/HeaderCartButton.js
+++ Section-11__Project-foodOrderingApp/src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,37 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import CartContext from "../../store/cart-context"
 import CartIcon from '../Cart/CartIcon'
 
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = (props) => {
-
+    const [btnIsActived, setBtnIsActived] = useState(false)
+    
     const cartContext = useContext(CartContext)
-
-    const cartItems = cartContext.items.reduce((curNumber, item) => {
+    
+    const {items} = cartContext
+    const cartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount
     }, 0)
+    
+    const btnClasses = `${classes.button} ${btnIsActived ? classes.bump : ''}`
 
+    useEffect(() => {
+        if (items.length  === 0) {
+            return;
+        }
+        setBtnIsActived(true)
+        const timer = setTimeout(() => {
+            setBtnIsActived(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+    
     return (
-        <button className={classes.button} onClick={props.onClickCart}>
+        <button className={btnClasses} onClick={props.onClickCart}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
