fc4ff314fcd8c37772db4da9029babab4f4a76a8
devangga <devanggakertawijaya@gmail.com>
2022-10-18 11:28:43 +0700

    Components updates in action

--- Section-12__reactBehindTheScenes__OptimizationTechniques/src/App.js
+++ Section-12__reactBehindTheScenes__OptimizationTechniques/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button'
 
 function App() {
+
+  const [showParagraph, setShowparagraph] = useState(false)
+
+  const onclickHandler = () => {
+    setShowparagraph((prevShowParagraph) => 
+      !prevShowParagraph
+    )
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {
+        showParagraph && <p>This is new !!</p>
+      }
+      <Button onClick={onclickHandler}>Show Paragraph</Button>
     </div>
   );
 }
