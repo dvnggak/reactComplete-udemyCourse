2c22635686a41a8428b3843482452c14af50e066
devangga <devanggakertawijaya@gmail.com>
2022-10-18 11:48:24 +0700

    Child Component Re-Evaluation

--- Section-12__reactBehindTheScenes__OptimizationTechniques/src/App.js
+++ Section-12__reactBehindTheScenes__OptimizationTechniques/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button'
 
 function App() {
@@ -16,9 +17,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {
-        showParagraph && <p>This is new !!</p>
-      }
+      <DemoOutput show={showParagraph}/>
       <Button onClick={onclickHandler}>Show Paragraph</Button>
     </div>
   );
