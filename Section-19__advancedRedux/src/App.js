e4c545675c269fc0ef9fc09cc3c2aa41b7bae75b
devangga <devanggakertawijaya@gmail.com>
2022-11-28 10:27:28 +0700

    App.js: conditional rendering on Cart.js

--- Section-19__advancedRedux/src/App.js
+++ Section-19__advancedRedux/src/App.js
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+
+  const showCart = useSelector( (state) => state.ui.cartIsShown)
+
   return (
     <Layout>
-      <Cart />
+      {
+        showCart ? <Cart /> : null
+      }
       <Products />
     </Layout>
   );
