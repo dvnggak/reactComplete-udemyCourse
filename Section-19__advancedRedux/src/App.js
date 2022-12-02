21e177ac452584c01c296ecaddb0fe20cea15acc
devangga <devanggakertawijaya@gmail.com>
2022-12-02 15:54:42 +0700

    action creator thunk

--- Section-19__advancedRedux/src/App.js
+++ Section-19__advancedRedux/src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true
 
@@ -17,42 +17,13 @@ function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Pending...',
-        message: 'Sending cart data!'
-      }))
-      const response = await fetch(
-        'https://react-http-92073-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', 
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed sending cart data')
-        }
-
-        dispatch(uiActions.showNotification({
-          status: 'success',
-          title: 'Success...',
-          message: 'Sending cart data successfully'
-        }))
-    }
-    
     if(isInitial) {
       isInitial = false
       return;
-    }
+    };
 
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error...',
-        message: 'Sending cart data Failed!'
-      }))
-    })
+    dispatch(sendCartData(cart))
+
   },[cart, dispatch])
 
   return (
--- Section-19__advancedRedux/src/store/cart-slice.js
+++ Section-19__advancedRedux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -40,6 +41,48 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Pending...',
+      message: 'Sending cart data!'
+    }))
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-http-92073-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', 
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        });
+    
+        if (!response.ok) {
+          throw new Error('Failed sending cart data')
+        }
+    }
+
+    try {
+      await sendRequest()
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success...',
+        message: 'Sending cart data successfully'
+      }))
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error...',
+        message: 'Sending cart data Failed!'
+      }))
+    }
+
+    
+
+  }
+}
 
 export const cartAction = cartSlice.actions;
 
