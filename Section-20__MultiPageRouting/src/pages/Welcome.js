7d5c7831444fd060e6f6abb0857818cb181451ed
devangga <devanggakertawijaya@gmail.com>
2022-12-06 11:08:04 +0700

    nesed route

--- Section-20__MultiPageRouting/src/pages/Welcome.js
+++ Section-20__MultiPageRouting/src/pages/Welcome.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
   return (
-    <h1>The Welcome Page</h1>
-  )
-}
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <h1>Welcome, new user !!</h1>
+      </Route>
+    </section>
+  );
+};
 
-export default Welcome
+export default Welcome;
