286b62a2fecf5486ce6913630499a6df12ca70c1
devangga <devanggakertawijaya@gmail.com>
2022-12-06 15:14:50 +0700

    outputting data o details page

--- Section-20__MultiPageRouting/practice1/src/pages/QuoteDetail.js
+++ Section-20__MultiPageRouting/practice1/src/pages/QuoteDetail.js
@@ -2,14 +2,33 @@ import React, { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Dev",
+    text: "I am a React developer !!",
+  },
+  {
+    id: "q2",
+    author: "Deva",
+    text: "I am a Front-End Engineer",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found !</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>;
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
