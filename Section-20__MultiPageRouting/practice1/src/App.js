6f3cd79883d1b0152f17ca494820f071e058c1e2
devangga <devanggakertawijaya@gmail.com>
2022-12-06 11:49:52 +0700

    extracting params

--- Section-20__MultiPageRouting/practice1/src/App.js
+++ Section-20__MultiPageRouting/practice1/src/App.js
@@ -1,11 +1,15 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
     <Switch>
-      <Route path="/quotes">
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
         <AllQuotes />
       </Route>
       <Route path="/quotes/:quoteId">
