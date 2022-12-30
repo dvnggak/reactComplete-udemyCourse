fe076ed8f91943fcf0c144e0ed2ce35029557ef0
dvnggak <devanggakertawijaya@gmail.com>
2022-12-30 14:51:39 +0700

    Linking between page

--- Section-23__introductionWithNextJS/nextjs-materials/pages/news/index.js
+++ Section-23__introductionWithNextJS/nextjs-materials/pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is aa great Framework
+          </Link>
+        </li>
+        <li>NextJS is aa hard Framework</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
