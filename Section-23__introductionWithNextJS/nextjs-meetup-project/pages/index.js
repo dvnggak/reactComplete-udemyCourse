7fc9dffaaf9f8d563c90c339666c6ff582c5dcab
dvnggak <devanggakertawijaya@gmail.com>
2022-12-30 16:15:31 +0700

    Outputting listof meetups

--- Section-23__introductionWithNextJS/nextjs-meetup-project/pages/index.js
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/index.js
@@ -1,5 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    Image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg",
+    address: "Musée du Louvre (Paris, France)",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    Image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/2018_01_%28Blue%29_-_Chaouen.jpg/1024px-2018_01_%28Blue%29_-_Chaouen.jpg",
+    address: "Chefchaouen, Marrakesh, Morocco",
+    description: "This is a second meetup",
+  },
+];
+
 const HomePage = () => {
-  return <div>index</div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
