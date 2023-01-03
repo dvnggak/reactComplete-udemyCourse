ba5626d0f789a17d0b5e13202571cd70f49d3cd9
dvnggak <devanggakertawijaya@gmail.com>
2023-01-03 10:09:34 +0700

    adding new meetup form

--- Section-23__introductionWithNextJS/nextjs-meetup-project/pages/index.js
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/index.js
@@ -4,7 +4,7 @@ const DUMMY_MEETUPS = [
   {
     id: "m1",
     title: "A First Meetup",
-    Image:
+    image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg",
     address: "Musée du Louvre (Paris, France)",
     description: "This is a first meetup",
@@ -12,7 +12,7 @@ const DUMMY_MEETUPS = [
   {
     id: "m2",
     title: "A Second Meetup",
-    Image:
+    image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/2018_01_%28Blue%29_-_Chaouen.jpg/1024px-2018_01_%28Blue%29_-_Chaouen.jpg",
     address: "Chefchaouen, Marrakesh, Morocco",
     description: "This is a second meetup",
--- Section-23__introductionWithNextJS/nextjs-meetup-project/pages/new-meetup/index.js
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 // domain-name.com/new-meetup
+import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetUpPage = () => {
+  const onAddMeetUpHandler = (enteredMeetUpData) => {
+    console.log(enteredMeetUpData);
+  };
+
+  return <NewMeetUpForm onAddMeetup={onAddMeetUpHandler} />;
+};
+
+export default NewMeetUpPage;
