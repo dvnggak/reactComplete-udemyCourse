708d4e0c3865bd3d3681b53c271111c5295010af
dvnggak <devanggakertawijaya@gmail.com>
2023-01-03 10:45:28 +0700

    ading custom component for showing meetup details

---
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/[meetupid]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg"
+      title="A First Meetup"
+      address="Some street5, some city"
+      description="This is a first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
