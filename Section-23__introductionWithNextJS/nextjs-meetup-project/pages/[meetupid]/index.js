994de6f133d3bac7df25a79d193f2d955efa8187
dvnggak <devanggakertawijaya@gmail.com>
2023-01-09 11:40:15 +0700

    getStaticPaths

--- Section-23__introductionWithNextJS/nextjs-meetup-project/pages/[meetupid]/index.js
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/[meetupid]/index.js
@@ -11,4 +11,41 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupid: "m1",
+        },
+      },
+      {
+        params: {
+          meetupid: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // Fetch data from an API
+
+  const meetupId = context.params.meetupid;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Le_Louvre_-_Aile_Richelieu.jpg/800px-Le_Louvre_-_Aile_Richelieu.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some street5, some city",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
