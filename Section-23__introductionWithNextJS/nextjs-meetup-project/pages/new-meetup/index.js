5c71dacd01731e3c8cfd238e8cf15d58ccf07fd2
dvnggak <devanggakertawijaya@gmail.com>
2023-01-11 10:22:42 +0700

    sending http PoST req to API

--- Section-23__introductionWithNextJS/nextjs-meetup-project/pages/new-meetup/index.js
+++ Section-23__introductionWithNextJS/nextjs-meetup-project/pages/new-meetup/index.js
@@ -1,12 +1,26 @@
 // domain-name.com/new-meetup
+import { useRouter } from "next/router";
 import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
 
-const NewMeetUpPage = () => {
-  const onAddMeetUpHandler = (enteredMeetUpData) => {
-    console.log(enteredMeetUpData);
-  };
+function NewMeetUpPage() {
+  const router = useRouter();
+
+  async function onAddMeetUpHandler(enteredMeetUpData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetUpData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
+  }
 
   return <NewMeetUpForm onAddMeetup={onAddMeetUpHandler} />;
-};
+}
 
 export default NewMeetUpPage;
