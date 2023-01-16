82cdecff3dd05738f74c424adec468d7b3c2894c
dvnggak <devanggakertawijaya@gmail.com>
2023-01-16 14:46:28 +0700

    wrapping modal compnent with Transitions

--- Section-24__animatingReactApps/src/components/Modal/Modal.js
+++ Section-24__animatingReactApps/src/components/Modal/Modal.js
@@ -1,24 +1,30 @@
 import React from "react";
+import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasess = [
-    "Modal",
-    props.show === "entering"
-      ? "ModalOpen"
-      : props.show === "exiting"
-      ? "ModalClosed"
-      : null,
-  ];
-
   return (
-    <div className={cssClasess.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+      {(state) => {
+        const cssClasess = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasess.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
