f53f48200fc33caa1f25b7d79594a7c776ba9d0a
dvnggak <devanggakertawijaya@gmail.com>
2023-01-20 10:08:23 +0700

    grouing test case

--- Section-26__testingReactApps/src/components/Greeting.test.js
+++ Section-26__testingReactApps/src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("render Hello World as a text", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting Component", () => {
+  test("render Hello World as a text", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  //...NOTHING
+    //Act
+    //...NOTHING
 
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World");
-  expect(helloWorldElement).toBeInTheDocument();
+    //Assert
+    const helloWorldElement = screen.getByText("Hello World");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
